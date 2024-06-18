523bd8156e5b9d01377820cdbec41eeee694eb34
kaiwen <kaiwen073c@gmail.com>
2024-06-18 14:30:45 +0800

    小修正

--- index.js
+++ index.js
@@ -12,63 +12,11 @@ const bot = linebot({
 })
 
 // 當機器人收到訊息時，執行{}內的程式碼
-// event 包含了訊息的類型、文字等（待編輯）
 bot.on('message', event => {
-  // process.env.DEBUG的true為文字，要用''框起來
   if (process.env.DEBUG === 'true') {
     console.log(event)
   }
-  if (event.message.type === 'text') {
-    // event.message.text為使用者傳送的文字
-    if (event.message.text === '前端') {
-      // commandFE()待確認
-      commandFE(event)
-    } else if (event.message.text === 'usd') {
-      // 執行command>usd.js裡的函數
-      commandUsd(event)
-    } else if (event.message.text === 'qr') {
-      // 這裡是quick reply
-      // event.reply 為機器人回覆的訊息
-      event.reply({
-        type: 'text',
-        text: '123',
-        quickReply: {
-          items: [
-            {
-              type: 'action',
-              action: {
-                type: 'message',
-                // 按下去使用者會傳送出的文字
-                text: 'ubike:taipei',
-                // 按鈕文字
-                label: 'taipei'
-              }
-            },
-            {
-              type: 'action',
-              action: {
-                type: 'uri',
-                uri: 'https://wdaweb.github.io',
-                label: '職訓'
-              }
-            },
-            {
-              type: 'action',
-              action: {
-                // 使使用者傳送的訊息不會出現在聊天室
-                type: 'postback',
-                uri: 'https://wdaweb.github.io',
-                label: '職訓',
-                data: 'aaa'
-              }
-            }
-          ]
-        }
-      })
-    }
-  } else if (event.message.type === 'location') {
-    // commandTWGod待確認
-    // commandTWGod(event)
+  if (event.message.type === 'location') {
     commandPark(event)
   }
 })
